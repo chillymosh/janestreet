fd318239b87871532629ced990824e2bd8d667a7
chillymosh <86857777+chillymosh@users.noreply.github.com>
2024-01-19 11:59:26 +0000

    Defaultdict version

--- graph-nodes-alt.py
+++ graph-nodes-alt.py
@@ -1,54 +1,34 @@
 from __future__ import annotations
+from collections import defaultdict
 from queue import Queue
-
-"""
-example facts:
-    m = 3.28 ft
-    ft = 12 in
-    hr = 60 min
-    min = 60 sec
-
- example queries:
-    2 m = ? in --> answer = 78.72
-    13 in = ? m --> answer = 0.330 (roughly)
-    13 in = ? hr --> 'not convertible!'
-"""
-
 
 Fact = tuple[str, float | int, str]
 Query = tuple[float | int, str, str]
 
-
 class Node:
-    def __init__(self, unit: str) -> None:
-        self.unit: str = unit
+    def __init__(self, unit: str = "") -> None:
+        self.unit = unit
         self.edges: list[Edge] = []
 
     def add_edge(self, multiplier: float, other_node: Node) -> None:
-        edge = Edge(multiplier, other_node)
-        self.edges.append(edge)
-
+        if multiplier != 0:
+            self.edges.append(Edge(multiplier, other_node))
 
 class Edge:
     def __init__(self, multiplier: float, node: Node):
         self.multiplier = multiplier
         self.node = node
 
-
 def parse_facts(facts: list[Fact]) -> dict[str, Node]:
-    name_to_node: dict[str, Node] = {}
+    name_to_node:defaultdict[str, Node] = defaultdict(Node)
+    
     for left_unit, multiplier, right_unit in facts:
-        if left_unit not in name_to_node:
-            left_node = Node(left_unit)
-            name_to_node[left_unit] = left_node
-        if right_unit not in name_to_node:
-            right_node = Node(right_unit)
-            name_to_node[right_unit] = right_node
-        name_to_node[left_unit].add_edge(multiplier, name_to_node[right_unit])
-        name_to_node[right_unit].add_edge(1 / multiplier, name_to_node[left_unit])
+        left_node = name_to_node[left_unit]
+        right_node = name_to_node[right_unit]
+        left_node.add_edge(multiplier, right_node)
+        right_node.add_edge(1 / multiplier, left_node)
 
     return name_to_node
-
 
 def answer_query(query: Query, facts: dict[str, Node]) -> float | None:
     starting_amount, from_unit, to_unit = query
@@ -58,7 +38,7 @@ def answer_query(query: Query, facts: dict[str, Node]) -> float | None:
     from_node = facts[from_unit]
     to_node = facts[to_unit]
 
-    to_visit: Queue[tuple[Node, float]] = Queue()
+    to_visit: Queue[tuple[Node, int | float]] = Queue()
     to_visit.put((from_node, starting_amount))
     visited = {from_node}
     while not to_visit.empty():
@@ -73,7 +53,6 @@ def answer_query(query: Query, facts: dict[str, Node]) -> float | None:
                 to_visit.put((edge.node, with_latest_multiplier))
 
     return None
-
 
 facts = parse_facts([("m", 3.28, "ft"), 
                      ("ft", 12, "in"), 
