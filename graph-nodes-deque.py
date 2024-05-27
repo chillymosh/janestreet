f0d80c3acf659c06fbdf7efdb1fc7f6740f7c0f7
chillymosh <86857777+chillymosh@users.noreply.github.com>
2024-05-27 17:08:48 +0100

    Deque implementation

---
+++ graph-nodes-deque.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+from collections import deque
+
+"""
+example facts:
+    m = 3.28 ft
+    ft = 12 in
+    hr = 60 min
+    min = 60 sec
+
+ example queries:
+    2 m = ? in --> answer = 78.72
+    13 in = ? m --> answer = 0.330 (roughly)
+    13 in = ? hr --> 'not convertible!'
+"""
+
+
+Fact = tuple[str, float | int, str]
+Query = tuple[float | int, str, str]
+
+
+class Node:
+    def __init__(self, unit: str) -> None:
+        self.unit: str = unit
+        self.edges: list[Edge] = []
+
+    def add_edge(self, multiplier: float, other_node: Node) -> None:
+        edge = Edge(multiplier, other_node)
+        self.edges.append(edge)
+
+
+class Edge:
+    def __init__(self, multiplier: float, node: Node) -> None:
+        self.multiplier = multiplier
+        self.node = node
+
+
+def parse_facts(facts: list[Fact]) -> dict[str, Node]:
+    name_to_node: dict[str, Node] = {}
+    for left_unit, multiplier, right_unit in facts:
+        left_node = name_to_node.setdefault(left_unit, Node(left_unit))
+        right_node = name_to_node.setdefault(right_unit, Node(right_unit))
+        left_node.add_edge(multiplier, right_node)
+        right_node.add_edge(1 / multiplier, left_node)
+
+    return name_to_node
+
+
+def answer_query(query: Query, facts: dict[str, Node]) -> float | None:
+    starting_amount, from_unit, to_unit = query
+    if from_unit not in facts or to_unit not in facts:
+        return None
+
+    from_node = facts[from_unit]
+    to_node = facts[to_unit]
+
+    to_visit = deque([(from_node, starting_amount)])
+    visited = {from_node}
+    while to_visit:
+        current_node, current_amount = to_visit.popleft()
+        if current_node == to_node:
+            return current_amount
+
+        for edge in current_node.edges:
+            if edge.node not in visited:
+                visited.add(edge.node)
+                with_latest_multiplier = current_amount * edge.multiplier
+                to_visit.append((edge.node, with_latest_multiplier))
+
+    return None
+
+
+facts = parse_facts([("m", 3.28, "ft"), 
+                     ("ft", 12, "in"), 
+                     ("hr", 60, "min"), 
+                     ("min", 60, "sec")])
+
+queries = [(2, "m", "in"), 
+           (13, "in", "m"), 
+           (13, "in", "hr")]
+
+for query in queries:
+    result = answer_query(query, facts)
+    if result is None:
+        print(f"{query[0]} {query[1]} = not convertible!")
+    else:
+        print(f"{query[0]} {query[1]} = {result:.3f} {query[2]}")
